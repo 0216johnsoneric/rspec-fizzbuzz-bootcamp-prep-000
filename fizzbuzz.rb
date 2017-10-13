fbc79e7dccacbb3de7ac400c0b734ccdf7f6f9b1
Eric Johnson <0216johnsoneric@gmail.com>
2017-10-13 23:28:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -11,12 +11,28 @@
 #    return "Fizz"
 # end
 
+# def fizzbuzz(num)
+#   if (num % 3 == 0) && (num % 5 == 0)
+#     "FizzBuzz"
+#   elsif num % 3 == 0
+#     "Fizz"
+#   elsif num % 5 == 0
+#     "Buzz"
+#   end
+# end
+
 def fizzbuzz(num)
-  if (num % 3 == 0) && (num % 5 == 0)
+  if num % 3 == 0
+    time3 = true
+  end
+  if num % 5 == 0
+    time5 = true
+  end
+  if (time3 && time5)
     "FizzBuzz"
-  elsif num % 3 == 0
+  elsif (time3)
     "Fizz"
-  elsif num % 5 == 0
+  elsif (time5)
     "Buzz"
   end
 end
